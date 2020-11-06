14c3b0ef884879940092730a51ec05373172572a
Darren <darrenmcc22@gmail.com>
2020-11-06 16:01:56 +0000

    import FlipCardBack / Front Components in module

--- src/app/flip-card/flip-card.module.ts
+++ src/app/flip-card/flip-card.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlipCardComponent } from './flip-card.component';
+import { FlipCardFrontComponent} from './flip-card-front';
+import { FlipCardBackComponent } from './flip-card-back';
 
 
 
 @NgModule({
-  declarations: [FlipCardComponent],
+  declarations: [FlipCardComponent, FlipCardBackComponent, FlipCardFrontComponent],
   imports: [
     CommonModule
   ],
-  exports: [FlipCardComponent]
+  exports: [FlipCardComponent, FlipCardBackComponent, FlipCardFrontComponent]
 })
 export class FlipCardModule { }
